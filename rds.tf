c5a1716192052701998a09fe928aaef1419bc828
ABE19950311 <abe19950311@gmail.com>
2024-10-01 21:13:04 +0900

    route53

--- rds.tf
+++ rds.tf
@@ -1,33 +1,33 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance
-resource "aws_db_instance" "rds" {
-  allocated_storage       = 10
-  backup_retention_period = 7
-  db_subnet_group_name    = aws_db_subnet_group.dbsubnet.name
-  vpc_security_group_ids  = [aws_security_group.rds.id]
-  engine                  = "mysql"
-  engine_version          = "8.0.35"
-  instance_class          = "db.t3.micro"
-  multi_az                = true
-  password                = "hogehoge"
-  username                = "hogeUser"
-  storage_encrypted       = true
-  skip_final_snapshot     = true
+# resource "aws_db_instance" "rds" {
+#   allocated_storage       = 10
+#   backup_retention_period = 7
+#   db_subnet_group_name    = aws_db_subnet_group.dbsubnet.name
+#   vpc_security_group_ids  = [aws_security_group.rds.id]
+#   engine                  = "mysql"
+#   engine_version          = "8.0.35"
+#   instance_class          = "db.t3.micro"
+#   multi_az                = true
+#   password                = "hogehoge"
+#   username                = "hogeUser"
+#   storage_encrypted       = true
+#   skip_final_snapshot     = true
 
-  timeouts {
-    create = "3h"
-    delete = "3h"
-    update = "3h"
-  }
-}
+#   timeouts {
+#     create = "3h"
+#     delete = "3h"
+#     update = "3h"
+#   }
+# }
 
 #https://qiita.com/sicksixrock66/items/63c6b651e6ccc28b0285
 #https://zenn.dev/suganuma/articles/fe14451aeda28f
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_subnet_group
-resource "aws_db_subnet_group" "dbsubnet" {
-  name       = "main"
-  subnet_ids = [aws_subnet.rds1.id, aws_subnet.rds2.id]
+# resource "aws_db_subnet_group" "dbsubnet" {
+#   name       = "main"
+#   subnet_ids = [aws_subnet.rds1.id, aws_subnet.rds2.id]
 
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
+#   tags = {
+#     Name = "My DB subnet group"
+#   }
+# }
--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
 #https://zenn.dev/kou_kawa/articles/19-terraform-aws-route53
 resource "aws_route53_zone" "dev" {
-  name = "dev.example.com"
+  name = "hogestudy.com"
 
   tags = {
     Environment = "dev"
@@ -9,9 +9,9 @@ resource "aws_route53_zone" "dev" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
+  zone_id = aws_route53_zone.dev.zone_id
+  name    = "www.hogestudy.com"
   type    = "A"
   ttl     = 300
-  records = [aws_eip.lb.public_ip]
+  records = ["18.183.132.105"]
 }
